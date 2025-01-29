f6c144a24412cad8f21e0a6382bb65d79d4b15d4
AlinaPerf <alinaperfileva44@gmail.com>
2025-01-29 23:29:11 +0300

    Add files via upload

---
+++ консолька/консолька/консолька.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+// Функция для обмена значениями (передаем сюда ссылки)
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
+
+void move(int& x, int& y, int dx, int dy) {
+    x += dx;
+    y += dy;
+}
+
+int main() {
+    //Обмен значений
+    setlocale(0, "ru");
+    int a = 5, b = 10;
+    std::cout << "До обмена: a = " << a << ", b = " << b << std::endl;
+    swap(a, b);
+    std::cout << "После обмена: a = " << a << ", b = " << b << std::endl;
+
+    //Работа с координатами
+    int x = 3, y = 4;
+    std::cout << "Координаты точки до перемещения: (" << x << ", " << y << ")" << std::endl;
+    move(x, y, 2, 3);
+    std::cout << "Координаты точки после перемещения: (" << x << ", " << y << ")" << std::endl;
+
+    return 0;
+}
